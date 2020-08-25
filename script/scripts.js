c63e0b0f738216f7394d6e06d474177c8a02d71c
prjct0192 <project.nemesis92@gmail.com>
2020-08-25 18:39:53 +0300

    yobani rot

--- script/scripts.js
+++ script/scripts.js
@@ -1,21 +1,52 @@
 var $start = document.querySelector('#start');
 var $game = document.querySelector('#game');
-
+var $time = document.querySelector('#time');
+var $result = document.querySelector('#result');
+var $timeHeader = document.querySelector('#time-header');
+var $resultHeader = document.querySelector('#result-header');
 
 var score = 0
+var isGameStarted = false;
 $start.addEventListener('click', startGame);
 $game.addEventListener('click', handleBoxClick);
 
 function startGame() {
-
+    isGameStarted = true;
     $start.classList.add('hide');
     $game.style.backgroundColor = '#fff';
+
+
+    var interval = setInterval(function () {
+        var time = parseFloat($time.textContent);
+
+        if (time <= 0){
+            clearInterval(interval);
+        }else {
+            $time.textContent = (time - 0.1).toFixed(1)
+        }
+    }, 100)
 
     renderBox();
 
 }
 
+function setGameScore() {
+    $result.textContent = score.toString();
+}
+function endGame() {
+    isGameStarted = false;
+    setGameScore();
+    $start.classList.remove('hide');
+    $game.innerHTML = '';
+    $game.style.backgroundColor = '#ccc';
+    $timeHeader.classList.add('hide');
+    $resultHeader.classList.remove('hide');
+}
+
 function handleBoxClick(event) {
+    if (isGameStarted) {
+        return
+    }
     if (event.target.dataset.box) {
         score++;
         renderBox();
